30b2eb8f5c0251ec6e8078ee744b5b153ce3163a
gonkov <115842637+gonkov@users.noreply.github.com>
2022-10-22 19:30:06 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+
+
+        public static void Main()
+        {
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+            Sorting(a);
+            foreach (int item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+        public static void Sorting(List<int> a)
+        {
+            a.Add(1);
+            if (a.Count < 10)
+            {
+                Sorting(a);                
+            }
+
+        }
+        
+
+
+    }
+}
